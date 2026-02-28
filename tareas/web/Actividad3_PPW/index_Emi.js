39c4224b311c84a3d93bb265aa3cbda6d054a251
Em1Torres <emiliotorrescastillo06@gmail.com>
2026-02-28 16:30:52 -0600

    Subiendo archivos de Actividad 3 con screenshots

---
+++ tareas/web/Actividad3_PPW/index_Emi.js
@@ -0,0 +1,10 @@
+function mates() {
+    document.getElementById("par1").innerHTML = "Leibniz hizo aportaciones fundamentales al desarrollo del calculo diferencial e integral. Su principal contribucion fue la notacion simbolica, como el uso de la d para derivadas y el signo de integral, que permitio expresar operaciones complejas de manera clara y sistematica. Esta sintaxis se convirtio en el estandar universal y sigue siendo utilizada hoy en dia en matematicas, fisica e ingenieria.";
+}
+
+function integrals() {
+    let img = document.getElementById("image");
+    img.src = "https://thumbs.dreamstime.com/z/calculus-law-theory-mathematical-formula-equation-doodle-ha-handwriting-icon-blackboard-background-hand-drawn-model-56586258.jpg";
+    img.style.width = "350px"; 
+    img.style.height = "auto";
+}
