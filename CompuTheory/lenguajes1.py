4ceef604825bdec99d82ddd6bb6ee0dc23f41923
Em1Torres <emiliotorrescastillo06@gmail.com>
2026-04-06 09:49:02 -0600

    Creacion de carpeta de programas para clase de victor y el primer programa

---
+++ CompuTheory/lenguajes1.py
@@ -0,0 +1,36 @@
+def reconoce (w):
+    estado = 0
+    for c in w:
+
+        if estado == 0:
+            if c == 'a':
+                estado = 0
+            elif c == 'b':
+                estado = 1
+            else:
+                return False
+            
+        elif estado == 1:
+            if c == 'a':
+                estado = 0
+            elif c == 'b':
+                estado = 2
+            else:
+                return False
+            
+        elif estado == 2:
+            if c == '$':
+                return True
+            elif c == 'a':
+                estado = 2
+            elif c == 'b':
+                estado = 2
+            else:
+                return False
+            
+
+ws = ["bb$","ababbab$","abab$", "aaababa$","aaabbacba$"]
+
+for w in ws:
+    print(reconoce(w))
+
